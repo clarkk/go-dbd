f7bd6499cf97a84e35ea4fd7be51116939d4829c
clarkk <clark@electrobeat.dk>
2025-01-16 08:33:34 +0000

    update

--- pool.go
+++ pool.go
@@ -48,11 +48,11 @@ func Ping() bool {
 func Query_row(ctx context.Context, query sqlc.SQL, scan []any) error {
 	sql, err := query.Compile()
 	if err != nil {
-		return &Error{"DB query row compile: "+err.Error(), errors.Wrap(err, 0).ErrorStack()}
+		return &Error{"DB query row compile: "+sqlc.SQL_debug(query)+err.Error(), errors.Wrap(err, 0).ErrorStack()}
 	}
 	
 	if err := db.QueryRowContext(ctx, sql, query.Data()...).Scan(scan...); err != nil {
-		msg := "DB query row: "+err.Error()
+		msg := "DB query row: "+sqlc.SQL_debug(query)+err.Error()
 		stack := errors.Wrap(err, 0).ErrorStack()
 		if ctx_canceled(err) {
 			return &Timeout_error{msg, stack}
@@ -66,11 +66,11 @@ func Insert(ctx context.Context, query sqlc.SQL) (int, error){
 	var id int
 	sql, err := query.Compile()
 	if err != nil {
-		return id, &Error{"DB insert compile: "+err.Error(), errors.Wrap(err, 0).ErrorStack()}
+		return id, &Error{"DB insert compile: "+sqlc.SQL_debug(query)+err.Error(), errors.Wrap(err, 0).ErrorStack()}
 	}
 	
 	if err := db.QueryRowContext(ctx, sql+" RETURNING id", query.Data()...).Scan(&id); err != nil {
-		msg := "DB insert: "+err.Error()
+		msg := "DB insert: "+sqlc.SQL_debug(query)+err.Error()
 		stack := errors.Wrap(err, 0).ErrorStack()
 		if ctx_canceled(err) {
 			return 0, &Timeout_error{msg, stack}
