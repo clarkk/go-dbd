b0c46bc6f67d4253a9183a36dc816efc48b990bb
clarkk <clark@electrobeat.dk>
2025-07-17 14:28:23 +0000

    update

--- sqlc/query.go
+++ sqlc/query.go
@@ -56,7 +56,7 @@ func SQL_debug(q SQL) string {
 }
 
 func SQL_error(msg string, q SQL, err error) string {
-	return msg+"\nSQL-error: "+err.Error()+"\nSQL: "+SQL_debug(q)
+	return msg+"\n"+err.Error()+"\n"+SQL_debug(q)
 }
 
 func (q *query) Data() []any {
