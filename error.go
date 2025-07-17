097ac841d4d7fe2871ae8ebbaa946f4cba79a0d2
clarkk <clark@electrobeat.dk>
2025-07-17 13:01:05 +0000

    error

---
+++ error.go
@@ -0,0 +1,37 @@
+package dbd
+
+import (
+	"errors"
+	"context"
+	"database/sql"
+)
+
+var ErrNotFound = errors.New("Not found")
+
+type (
+	Error struct {
+		error 	string
+		stack 	string
+	}
+	
+	Timeout_error struct {
+		error 	string
+		stack 	string
+	}
+)
+
+func No_rows_error(err error) bool {
+	return errors.Is(err, sql.ErrNoRows)
+}
+
+func (e *Error) Error() string {
+	return e.error+": "+e.stack
+}
+
+func (e *Timeout_error) Error() string {
+	return e.error
+}
+
+func ctx_canceled(err error) bool {
+	return errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded)
+}
